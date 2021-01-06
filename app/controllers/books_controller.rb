b5c39f1b26dc2d1b58c78a1381d3ef59d744b5cf
EC2 Default User <ec2-user@ip-172-31-9-249.ec2.internal>
2021-01-06 09:18:13 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
   def create
-    @user = User.find(current_user.id)
+    @user = User.find(current_user.id)#ここ変えました
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
